d9378f2625e3a6223d78a7083936fdfc6887932b
Aashish <shivwatches110@gmail.com>
2025-07-06 18:13:05 +0530

    fixed 404 in new category again

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -1,6 +1,6 @@
 import ProductListing from '../page';
 import { getCategories } from '@/lib/supabase';
-
+export const dynamic = 'force-dynamic';
 export async function generateStaticParams() {
   const categories = await getCategories();
   
